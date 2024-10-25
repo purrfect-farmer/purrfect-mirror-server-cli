93f0d83edbb9132ff0173c2c8ecde059e6cab635
Sadiq Salau <sadiqsalau888@gmail.com>
2024-10-25 10:24:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const chalk = require("chalk");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { networkInterfaces } = require("os");
+
+function createSyncServer() {
+  return new Promise((resolve, reject) => {
+    const PORT = 7777;
+    const server = createServer();
+    const io = new Server(server, {
+      cors: {
+        origin: "*",
+      },
+    });
+
+    /** Add to Room */
+    io.on("connection", (socket) => {
+      socket.join("receivers");
+
+      socket.on("message", (arg) => {
+        socket.to("receivers").emit("command", arg);
+      });
+    });
+
+    /** Start Server */
+    server.on("error", reject).listen(PORT, (error) => {
+      if (error) return reject(error);
+
+      const nets = networkInterfaces();
+      const addresses = [];
+
+      for (const interfaces of Object.values(nets)) {
+        for (const net of interfaces) {
+          const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+          if (net.family === familyV4Value) {
+            addresses.push(`${net.address}:${PORT}`);
+          }
+        }
+      }
+
+      resolve({ io, server, addresses });
+    });
+  });
+}
+
+createSyncServer()
+  .then(({ addresses }) => {
+    console.log(chalk.bold.green("Sync Server is up and running\n"));
+
+    addresses.forEach((address) => {
+      console.log(`${chalk.blue("[ADDR]")} ${chalk.bold.yellow(address)}`);
+    });
+  })
+  .catch((e) => {
+    console.log(chalk.bold.red("Failed to Start Sync Server\n"));
+
+    console.log(
+      chalk.yellow(
+        "Please ensure the server isn't running already.\nYou should restart your device if the problem persists. \n"
+      )
+    );
+  });
